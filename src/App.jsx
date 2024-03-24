124f6e9be7bec64e18f889592abc6ff4444d6c9e
aldipram_ <aldinanda@pop-os.localdomain>
2024-03-24 20:52:43 +0700

    perbaikan homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,9 @@ function App() {
 
   return (
     <>
-    <div className="bg-gradient-to-r from-[#ffd7af7a] to-[#965b208e]">
       <div className="max-w-[500px] mx-auto">
           <HomePage />
       </div>
-    </div>
-    
     </>
   )
 }
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -8,35 +8,37 @@ import MarqueeText from "../utils/TeksBerjalan/TeksBerjalan";
 
 const HomePage = () => {
 
-  const { isPlaying, togglePlay } = useContext(MusicContext);1
+  const { isPlaying, togglePlay } = useContext(MusicContext);
   const songTitle = "So This Is Love (feat. ConTejas).mp3";
   
   return (
-      <section  data-aos="fade-in" data-aos-duration="1000" className="bg-homePage bg-center bg-no-repeat bg-cover">
-        <div className="flex flex-col items-center justify-center h-screen gap-5 pt-20 shadow-xl shadow-black">
-          <h1 data-aos="zoom-in-up" data-aos-duration="1000" className="font-poppins text-[20px] font-semibold">
-            The Wedding of
-          </h1>
+      <section className="bg-gradient-to-r from-[#ffd7af7a] to-[#965b208e]">
+        <div data-aos="fade-in" data-aos-duration="1000" className="bg-homePage bg-center bg-no-repeat bg-cover">
+          <div className="flex flex-col items-center justify-center h-screen gap-5 pt-20 shadow-xl shadow-black">
+            <h1 data-aos="zoom-in-up" data-aos-duration="1000" className="font-poppins text-[20px] font-semibold">
+              The Wedding of
+            </h1>
 
-          <div className="flex flex-col items-center font-playfair text-bold sm:text-4xl text-primary gap-3 tracking-wider py-5">
-            <p data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100" className="text-4xl">Shindy</p>
-            <span data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" className="text-xl font-poppins">and</span>
-            <p data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100" className="text-4xl">Ruhiyat</p>
-          </div>
+            <div className="flex flex-col items-center font-playfair text-bold sm:text-4xl text-primary gap-3 tracking-wider py-5">
+              <p data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100" className="text-4xl">Shindy</p>
+              <span data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" className="text-xl font-poppins">and</span>
+              <p data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100" className="text-4xl">Ruhiyat</p>
+            </div>
 
-          <Link to="/sambutan">
-            <button data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" className="font-poppins font-medium text-white py-2 px-4 bg-primary rounded-md tracking-wider">
-              Open
-            </button>
-          </Link>
-          <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1500" className="flex items-center gap-3 whitespace-nowrap bg-[#000000c5] rounded-md p-2 mt-5 w-[50%]">
-            <MarqueeText text={songTitle} />
-            <button
-              onClick={togglePlay}
-              className="bg-white rounded-full p-2"
-            >
-              {isPlaying ? <FaPause className="text-primary" size={20} /> : <FaPlay className="text-primary" size={20} />}
-            </button>
+            <Link to="/sambutan">
+              <button data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500" className="font-poppins font-medium text-white py-2 px-4 bg-primary rounded-md tracking-wider">
+                Open
+              </button>
+            </Link>
+            <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1500" className="flex items-center gap-3 whitespace-nowrap bg-[#000000c5] rounded-md p-2 mt-5 w-[50%]">
+              <MarqueeText text={songTitle} />
+              <button
+                onClick={togglePlay}
+                className="bg-white rounded-full p-2"
+              >
+                {isPlaying ? <FaPause className="text-primary" size={20} /> : <FaPlay className="text-primary" size={20} />}
+              </button>
+            </div>
           </div>
         </div>
       </section>
